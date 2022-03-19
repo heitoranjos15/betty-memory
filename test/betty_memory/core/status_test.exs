578dd7ec963d219c41a008b04a9eefe7b4d9801a
heitoranjos15 <heitoranjos96@gmail.com>
2022-03-19 18:49:37 -0300

    chore: Adding more validation on status testing Also: - Map.put_new changed to Map.put

--- test/betty_memory/core/status_test.exs
+++ test/betty_memory/core/status_test.exs
@@ -1,12 +1,21 @@
 defmodule BettyMemory.Test.Core.StatusMock do
   use ExUnit.Case
+
+  import BettyMemory.Test.Seed.MarketSeed
+
   alias BettyMemory.Core.Status
+  alias BettyMemory.Core.Market
 
   describe "add_status/2" do
     test "should return entity with status" do
       entity = %{name: "entity"}
       _entity_with_status = %{name: "entity", status: 0}
       assert _entity_with_status = Status.add_status(entity, :inactive)
+    end
+
+    test "should return market with status" do
+      market = market_data()
+      assert %Market{status: 1} = Status.add_status(market, :active)
     end
   end
 end
